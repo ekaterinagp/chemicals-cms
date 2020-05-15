a1a66737e97190f4fef2e8c96848dc7d511281e8
ekaterinagp <katya.koriakina@gmail.com>
2020-05-15 09:54:57 +0200

    model warehouseitem fixed

--- server/models/WarehouseItem.js
+++ server/models/WarehouseItem.js
@@ -1,28 +1,19 @@
 const { Model } = require("objection");
 
-const Job = require("./Job");
 const Warehouse = require("./Warehouse");
 
-class JobItem extends Model {
+class WarehouseItem extends Model {
   static get tableName() {
-    return "jobItem";
+    return "warehouseitem";
   }
 
   static get relationMappings() {
     return {
-      job: {
-        relation: Model.HasOneRelation,
-        modelClass: Job,
-        join: {
-          from: "jobItem.job_id",
-          to: "job.id",
-        },
-      },
       warehouse: {
         relation: Model.HasOneRelation,
         modelClass: Warehouse,
         join: {
-          from: "jobItem.warehouse_id",
+          from: "warehouseitem.warehouse_id",
           to: "warehouse.id",
         },
       },
@@ -30,4 +21,4 @@ class JobItem extends Model {
   }
 }
 
-module.exports = JobItem;
+module.exports = WarehouseItem;
