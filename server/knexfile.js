232f96fd0ce7aed0a3d6ca529e48704003509e17
ekaterinagp <katya.koriakina@gmail.com>
2020-05-27 15:52:35 +0200

    db

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,28 +1,28 @@
 const credentials = require("./config/dbcredentials");
 const knexSnakeCaseMapper = require("objection").knexSnakeCaseMappers;
 
-module.exports = {
-  development: {
-    client: "mysql",
-    connection: {
-      host: "pqxt96p7ysz6rn1f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-      database: "jwfpzfz7zbu3zy2b",
-      user: "c1rsbutdqmetw7zx",
-      password: "mv880478nmm1tgks",
-    },
-  },
-  ...knexSnakeCaseMapper(),
-};
-
 // module.exports = {
 //   development: {
 //     client: "mysql",
 //     connection: {
-//       // host: "ekaterinagp.dk.mysql",
-//       database: credentials.database,
-//       user: credentials.user,
-//       password: credentials.password,
+//       host: "pqxt96p7ysz6rn1f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//       database: "jwfpzfz7zbu3zy2b",
+//       user: "c1rsbutdqmetw7zx",
+//       password: "mv880478nmm1tgks",
 //     },
 //   },
 //   ...knexSnakeCaseMapper(),
 // };
+
+module.exports = {
+  development: {
+    client: "mysql",
+    connection: {
+      host: "ekaterinagp.dk.mysql",
+      database: "ekaterinagp_dkchemicals",
+      user: "ekaterinagp_dkchemicals",
+      password: "123456",
+    },
+  },
+  ...knexSnakeCaseMapper(),
+};
